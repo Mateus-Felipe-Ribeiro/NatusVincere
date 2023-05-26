b3fcbd785a00d786b6c41e066b850d4b46b87cf8
Mateus Ribeiro <mateus@prs.net.br>
2023-05-26 17:35:01 -0300

    inicio estudo

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.inputRow}>
+        <TextInput placeholder="Digite um objetivo"></TextInput>
+        <Button title="+Objetivo"></Button>
+      </View>
+      <View>
+        <Text>Lista de Objetivos</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +18,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+  },
+  inputRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
   },
 });
